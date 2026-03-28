5456de4666cdc772117e3d94de67584495cd20e5
jahanviii <januraj2110@gmail.com>
2026-03-28 12:58:01 +0530

    second commit

--- model/login_model.py
+++ model/login_model.py
@@ -3,3 +3,7 @@ from pydantic import BaseModel
 class Login(BaseModel):
     Username: str
     Password: str
+
+    # jehfk
+    # jahanviiiiiiiiiiiiii 
+    
--- model/project_model.py
+++ model/project_model.py
@@ -14,3 +14,4 @@ class ProjectCreateModel(BaseModel):
 
 
 
+# jahanvi rajyaguru
